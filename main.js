7b13aab77fadf5957a4e74fde12c6216feea2591
Rayen Photo's <alenahuegar.1@gmail.com>
2023-09-26 23:57:50 -0300

    javascript sin terminar

---
+++ main.js
@@ -0,0 +1,55 @@
+let nombreCOMPLETO = prompt("ingrese su nombre completo ")
+ 
+alert("bienvenido " + nombreCOMPLETO)
+
+let prestamo = prompt( "ingrese el monto del prestamo que desea solicitar")
+
+if( prestamo<=500000) {
+    alert("el monto ingresado es valido")
+} else if(prestamo>500000){
+    alert("el monto ingresado excede nuestra disponibilidad")
+}
+
+for(let i=1 ; i<13 ; i++ ) {
+    alert("elija las cuotas en las que pagara el prestamo " +""+ i)
+    
+
+
+    
+    } if(prestamo<=500000) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
